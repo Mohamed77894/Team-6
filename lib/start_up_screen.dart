5e59e5a3121c96f09780a7e452c7bd7b55459a9e
Karim <karimahmedrageb27.6.2006@gmail.com>
2026-09-12 00:12:24 +0300

    basics

---
+++ lib/start_up_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class StartUpScreen extends StatelessWidget {
+  const StartUpScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+
+          ],
+        ),
+      ),
+    );
+  }
+}
